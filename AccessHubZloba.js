6d69b77b1a3c8b658cc12fc03cf1d049160f6f61
Martin Mečiar <mato.meciar@icloud.com>
2021-12-11 14:08:56 +0100

    Remove addition of other wizard buttons.
    Removed because the order of JSONs seemed to be broken.

--- AccessHubZloba.js
+++ AccessHubZloba.js
@@ -2,7 +2,7 @@
 // @name         AccessHub Zloba
 // @namespace    https://openuserjs.org/users/mato-meciar
 // @copyright    2019, mato-meciar (https://openuserjs.org/users/mato-meciar)
-// @version      0.4.4
+// @version      0.4.5
 // @description  Re-enables the Wizard functionality for creating/editing REST connections
 // @author       Martin Meciar
 // @license      MIT
@@ -27,169 +27,143 @@ $(document).ready(function () {
     const addWizzardButtons = function () {
       const connection = '<td style="width: 30%;"><span class="svicontext">ConnectionJSON</span><a href="javascript:;" class="btn svicon " onclick="restJsonFunction(\'ConnectionJSON\')"><i class="iconSVPAM-help svblue tooltips" data-container="body" data-placement="bottom" data-original-title="Populate ConnectionJSON"></i></a></td>'
       const connectionInfo = '<td style="width: 30%;"><a href="javascript:;" class="btn svicon " onclick="alert(connectionJsonString.replaceAll(\'<\', \'&amp;lt;\'))"><i class="icon-info svblue" data-container="body" data-placement="bottom" data-original-title="Populate ConnectionJSON"></i></a></td>'
-      const importUser = '<td style="width: 30%;"><span class="svicontext">ImportUserJSON</span><a href="javascript:;" class="btn svicon disableBtn" onclick="restJsonFunction(\'ImportUserJSON\')"><i class="iconSVPAM-help svblue tooltips" data-container="body" data-placement="bottom" data-original-title="Populate ImportUserJSON"></i></a></td>'
-      const importAccEnt = '<td style="width: 30%;"><span class="svicontext">ImportAccountEntJSON</span><a href="javascript:;" class="btn svicon disableBtn" onclick="restJsonFunction(\'ImportAccountEntJSON\')"><i class="iconSVPAM-help svblue tooltips" data-container="body" data-placement="bottom" data-original-title="Populate ImportAccountEntJSON"/></a></td>'
-      const createAcc = '<td style="width: 30%;"><span class="svicontext">CreateAccountJSON</span><a href="javascript:;" class="btn svicon disableBtn" onclick="restJsonFunction(\'CreateAccountJSON\')"><i class="iconSVPAM-help svblue tooltips" data-container="body" data-placement="bottom" data-original-title="Populate CreateAccountJSON"></i></a></td>'
-      const updateAcc = '<td style="width: 30%;"><span class="svicontext">UpdateAccountJSON</span><a href="javascript:;" class="btn svicon disableBtn" onclick="restJsonFunction(\'UpdateAccountJSON\')"><i class="iconSVPAM-help svblue tooltips" data-container="body" data-placement="bottom" data-original-title="Populate UpdateAccountJSON"></i></a></td>'
-      const enableAcc = '<td style="width: 30%;"><span class="svicontext">EnableAccountJSON</span><a href="javascript:;" class="btn svicon disableBtn" onclick="restJsonFunction(\'EnableAccountJSON\')"><i class="iconSVPAM-help svblue tooltips" data-container="body" data-placement="bottom"data-original-title="Populate EnableAccountJSON"></i></a></td>'
-      const disableAcc = '<td style="width: 30%;"><span class="svicontext">DisableAccountJSON</span><a href="javascript:;" class="btn svicon disableBtn" onclick="restJsonFunction(\'DisableAccountJSON\')"><i class="iconSVPAM-help svblue tooltips" data-container="body" data-placement="bottom"data-original-title="Populate DisableAccountJSON"></i></a></td>'
-      const addAccess = '<td style="width: 30%;"><span class="svicontext">AddAccessJSON</span><a href="javascript:;" class="btn svicon disableBtn" onclick="restJsonFunction(\'AddAccessJSON\')"><i class="iconSVPAM-help svblue tooltips" data-container="body" data-placement="bottom"data-original-title="Populate AddAccessJSON"></i></a></td>'
-      const removeAccess = '<td style="width: 30%;"><span class="svicontext">RemoveAccessJSON</span><a href="javascript:;" class="btn svicon disableBtn" onclick="restJsonFunction(\'RemoveAccessJSON\')"><i class="iconSVPAM-help svblue tooltips" data-container="body" data-placement="bottom"data-original-title="Populate RemoveAccessJSON"></i></a></td>'
-      const updateUser = '<td style="width: 30%;"><span class="svicontext">UpdateUserJSON </span><a href="javascript:;" class="btn svicon disableBtn" onclick="restJsonFunction(\'UpdateUserJSON\')"><i class="iconSVPAM-help svblue tooltips" data-container="body" data-placement="bottom" data-original-title="Populate UpdateUserJSON"></i></a></td>'
-      const changePass = '<td style="width: 30%;"><span class="svicontext">ChangePassJSON</span><a href="javascript:;" class="btn svicon disableBtn" onclick="restJsonFunction(\'ChangePassJSON\')"><i class="iconSVPAM-help svblue tooltips" data-container="body" data-placement="bottom"data-original-title="Populate ChangePassJSON"></i></a></td>'
-      const removeAcc = '<td style="width: 30%;"><span class="svicontext">RemoveAccountJSON</span><a href="javascript:;" class="btn svicon disableBtn" onclick="restJsonFunction(\'RemoveAccountJSON\')"><i class="iconSVPAM-help svblue tooltips" data-container="body" data-placement="bottom"data-original-title="Populate RemoveAccountJSON"></i></a></td>'
-      const ticketStatus = '<td style="width: 30%;"><span class="svicontext">TicketStatusJSON</span><a href="javascript:;" class="btn svicon disableBtn" onclick="restJsonFunction(\'TicketStatusJSON\')"><i class="iconSVPAM-help svblue tooltips" data-container="body" data-placement="bottom"data-original-title="Populate TicketStatusJSON"></i></a></td>'
-      const createTicket = '<td style="width: 30%;"><span class="svicontext">CreateTicketJSON</span><a href="javascript:;" class="btn svicon disableBtn" onclick="restJsonFunction(\'CreateTicketJSON\')"><i class="iconSVPAM-help svblue tooltips" data-container="body" data-placement="bottom"data-original-title="Populate CreateTicketJSON"></i></a></td>'
-  
+
       $("#showAdvanceConfigDiv > table > tbody > tr:nth-child(1) > td:nth-child(1)")[0].innerHTML = connection + connectionInfo
-      $("#showAdvanceConfigDiv > table > tbody > tr:nth-child(2) > td:nth-child(1)")[0].innerHTML = importUser
-      $("#showAdvanceConfigDiv > table > tbody > tr:nth-child(3) > td:nth-child(1)")[0].innerHTML = importAccEnt
-      $("#showAdvanceConfigDiv > table > tbody > tr:nth-child(4) > td:nth-child(1)")[0].innerHTML = createAcc
-      $("#showAdvanceConfigDiv > table > tbody > tr:nth-child(5) > td:nth-child(1)")[0].innerHTML = updateAcc
-      $("#showAdvanceConfigDiv > table > tbody > tr:nth-child(6) > td:nth-child(1)")[0].innerHTML = enableAcc
-      $("#showAdvanceConfigDiv > table > tbody > tr:nth-child(7) > td:nth-child(1)")[0].innerHTML = disableAcc
-      $("#showAdvanceConfigDiv > table > tbody > tr:nth-child(8) > td:nth-child(1)")[0].innerHTML = addAccess
-      $("#showAdvanceConfigDiv > table > tbody > tr:nth-child(9) > td:nth-child(1)")[0].innerHTML = removeAccess
-      $("#showAdvanceConfigDiv > table > tbody > tr:nth-child(10) > td:nth-child(1)")[0].innerHTML = updateUser
-      $("#showAdvanceConfigDiv > table > tbody > tr:nth-child(11) > td:nth-child(1)")[0].innerHTML = changePass
-      $("#showAdvanceConfigDiv > table > tbody > tr:nth-child(12) > td:nth-child(1)")[0].innerHTML = removeAcc
-      $("#showAdvanceConfigDiv > table > tbody > tr:nth-child(13) > td:nth-child(1)")[0].innerHTML = ticketStatus
-      $("#showAdvanceConfigDiv > table > tbody > tr:nth-child(14) > td:nth-child(1)")[0].innerHTML = createTicket
     };
-  
+
     addWizzardButtons();
-  
+
     var s = document.createElement("script");
     s.type = "text/javascript";
     s.innerHTML = `
   var connectionJsonMap = new Object();
   var smheight
   jQuery(document).ready(function() {
-  
+
       smheight=$(".aa_"+0).height();
       if(smheight <= 14){
           smheight = 49
       }
       $(".taheight_"+0).height(smheight)
-  
-  
+
+
       smheight=$(".aa_"+1).height();
       if(smheight <= 14){
           smheight = 49
       }
       $(".taheight_"+1).height(smheight)
-  
-  
+
+
       smheight=$(".aa_"+2).height();
       if(smheight <= 14){
           smheight = 49
       }
       $(".taheight_"+2).height(smheight)
-  
-  
+
+
       smheight=$(".aa_"+3).height();
       if(smheight <= 14){
           smheight = 49
       }
       $(".taheight_"+3).height(smheight)
-  
-  
+
+
       smheight=$(".aa_"+4).height();
       if(smheight <= 14){
           smheight = 49
       }
       $(".taheight_"+4).height(smheight)
-  
-  
+
+
       smheight=$(".aa_"+5).height();
       if(smheight <= 14){
           smheight = 49
       }
       $(".taheight_"+5).height(smheight)
-  
-  
+
+
       smheight=$(".aa_"+6).height();
       if(smheight <= 14){
           smheight = 49
       }
       $(".taheight_"+6).height(smheight)
-  
-  
+
+
       smheight=$(".aa_"+7).height();
       if(smheight <= 14){
           smheight = 49
       }
       $(".taheight_"+7).height(smheight)
-  
-  
+
+
       smheight=$(".aa_"+8).height();
       if(smheight <= 14){
           smheight = 49
       }
       $(".taheight_"+8).height(smheight)
-  
-  
+
+
       smheight=$(".aa_"+9).height();
       if(smheight <= 14){
           smheight = 49
       }
       $(".taheight_"+9).height(smheight)
-  
-  
+
+
       smheight=$(".aa_"+10).height();
       if(smheight <= 14){
           smheight = 49
       }
       $(".taheight_"+10).height(smheight)
-  
-  
+
+
       smheight=$(".aa_"+11).height();
       if(smheight <= 14){
           smheight = 49
       }
       $(".taheight_"+11).height(smheight)
-  
-  
+
+
       smheight=$(".aa_"+12).height();
       if(smheight <= 14){
           smheight = 49
       }
       $(".taheight_"+12).height(smheight)
-  
-  
+
+
       smheight=$(".aa_"+13).height();
       if(smheight <= 14){
           smheight = 49
       }
       $(".taheight_"+13).height(smheight)
-  
-  
+
+
       smheight=$(".aa_"+14).height();
       if(smheight <= 14){
           smheight = 49
       }
       $(".taheight_"+14).height(smheight)
-  
-  
+
+
       smheight=$(".aa_"+15).height();
       if(smheight <= 14){
           smheight = 49
       }
       $(".taheight_"+15).height(smheight)
-  
-  
-  
+
+
+
       App.init();
-  
+
       showAdvancedData('true') /*SD-5162*/
-  
+
       if('REST' == 'REST' && true){
-  
+
           clearSessionValue();
           setRestQuestionnaireSessionValue();
       }
   });
-  
+
   function clearSessionValue()
   {
       sessionStorage.entNamePageActive = 0
@@ -205,7 +179,7 @@ $(document).ready(function () {
       sessionStorage.accountEntJSON = '';
       sessionStorage.entitlementNameListMap = '';
   }
-  
+
   function connectionJsonDisableActivity() {
     if(isNullOrEmpty($("#ConnectionJSON").val())) {
       $('#externalconnectionattributevaluediv> div> table > tbody > tr:not(":first")').each(function(){
@@ -217,11 +191,11 @@ $(document).ready(function () {
       });
     }
   }
-  
+
   var connectionJsonString;
-  
+
   function setRestQuestionnaireSessionValue(){
-  
+
       if(!isNullOrEmpty($('#ConnectionJSON').val())){
           $.ajax({
               async:false,
@@ -249,7 +223,7 @@ $(document).ready(function () {
                sessionStorage.connectionDetails = connectionJsonString;
            }
       }
-  
+
       if(!isNullOrEmpty($('#ImportAccountEntJSON').val())){
           var ImportAccountEntJSONMap = new Object();
           ImportAccountEntJSONMap = JSON.parse($('#ImportAccountEntJSON').val())
@@ -267,14 +241,14 @@ $(document).ready(function () {
           sessionStorage.entitlementCount = entNo;
           sessionStorage.entitlementNameList = entNameList;
       }
-  
+
   }
-  
+
   function ButtonEnableFunction()
   {
       $('.disableBtn').attr('disabled', false);
   }
-  
+
   function isNullOrEmpty(obj) {
       var isNullStr = false;
       var objStr = obj;
@@ -283,8 +257,8 @@ $(document).ready(function () {
       }
       return isNullStr;
   }
-  
-  
+
+
   function restJsonFunction(fieldName)
   {
       var callActionName;
@@ -332,7 +306,7 @@ $(document).ready(function () {
           pageName = 'createJSONForCreateTicket';
           callActionName = 'createTicket';
       }
-  
+
       var pagePath = '';
       var importList = ["restJSONBasicQuestionnaire","createJSONForImportUser","restJSONEntitlementName","createJSONForCreateAccount"]
       var jsonDataStr = "";
@@ -378,7 +352,7 @@ $(document).ready(function () {
           else {
               pagePath = "/ECM/restQuestionnaire/restJSONQuestionnaire?pageName="+pageName+"&callActionName="+callActionName;
           }
-  
+
           completepath = pagePath,
           $('<div id="connJSON">').dialog({
               position: ['center', 70],
@@ -403,5 +377,6 @@ $(document).ready(function () {
   }
   `;
     $("head").append(s);
+    console.log('--- Raw Connection JSON ---');
+    console.log(sessionStorage.connectionDetails);
   });
-  
