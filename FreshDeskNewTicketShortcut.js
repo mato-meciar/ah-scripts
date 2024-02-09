b74b5808b404342a04eb95ad80dfce3a09f24e8e
Martin Mečiar <mato.meciar@icloud.com>
2024-02-09 15:28:33 +0100

    Update FreshDeskNewTicketShortcut.js

--- FreshDeskNewTicketShortcut.js
+++ FreshDeskNewTicketShortcut.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         FreshDesk New Ticket Shortcut
 // @namespace    https://openuserjs.org/users/mato-meciar
-// @copyright    2019, mato-meciar (https://openuserjs.org/users/mato-meciar)
-// @version      0.1
+// @copyright    2024, mato-meciar (https://openuserjs.org/users/mato-meciar)
+// @version      0.1.1
 // @description  Replace the Edit profile button with New ticket
 // @author       Martin Meciar
 // @license      MIT
@@ -10,7 +10,7 @@
 // @updateURL    https://raw.githubusercontent.com/mato-meciar/ah-scripts/master/FreshDeskNewTicketShortcut.js
 // @downloadURL  https://raw.githubusercontent.com/mato-meciar/ah-scripts/master/FreshDeskNewTicketShortcut.js
 // @supportURL   https://github.com/mato-meciar/ah-scripts
-// @match        *.freshdesk.com/support/*
+// @match        https://*.freshdesk.com/support/*
 // @grant        none
 // @run-at       document-body
 // ==/UserScript==
