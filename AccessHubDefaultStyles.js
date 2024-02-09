61b8483d2acb27dad8e9f17cba619c39c2da23cd
Martin Mečiar <mato.meciar@icloud.com>
2024-02-09 15:20:47 +0100

    Update AccessHubDefaultStyles.js

--- AccessHubDefaultStyles.js
+++ AccessHubDefaultStyles.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         AccessHub Default Styles
 // @namespace    https://openuserjs.org/users/mato-meciar
-// @copyright    2019, mato-meciar (https://openuserjs.org/users/mato-meciar)
-// @version      0.1
+// @copyright    2024, mato-meciar (https://openuserjs.org/users/mato-meciar)
+// @version      0.1.1
 // @description  Default CSS, welcome back
 // @author       Martin Meciar
 // @license      MIT
@@ -10,7 +10,7 @@
 // @updateURL    https://raw.githubusercontent.com/mato-meciar/ah-scripts/master/AccessHubDefaultStyles.js
 // @downloadURL  https://raw.githubusercontent.com/mato-meciar/ah-scripts/master/AccessHubDefaultStyles.js
 // @supportURL   https://github.com/mato-meciar/ah-scripts
-// @match        *.idaccesshub.com/ECM/*
+// @match        https://*.idaccesshub.com/ECM/*
 // @grant        none
 // @run-at       document-body
 // ==/UserScript==
